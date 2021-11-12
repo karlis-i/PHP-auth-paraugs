8bdee993c9a29c524da2310dd0b77745f541d468
Kārlis Immers <karlis.im@gmail.com>
2021-11-12 20:57:44 +0200

    Fixes for November 2021

--- application/index.php
+++ application/index.php
@@ -10,6 +10,12 @@ require_once('_header.php');
 
             <p>Lai pieslēgtos lapai, izmantojiet <a href="/login_form.php">šo formu</a>!</p>
 
-            <!-- <input type="text" value="<?php echo password_hash("guest", PASSWORD_BCRYPT); ?>"> -->
+            <input
+                type="text"
+                value="<?php echo password_hash("guest", PASSWORD_BCRYPT); ?>"
+                readonly
+                size="60"
+                style="font-family: monospace;"
+            >
 
 <?php require_once('_footer.php'); ?>
